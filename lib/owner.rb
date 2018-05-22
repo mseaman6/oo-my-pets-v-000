2fe5796c183d24c9a6020da8a6b7a831cd1b5c4a
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-22 17:47:54 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,7 +5,7 @@ class Owner
   def initialize(name)
     @name = name
     @species = "human"
-    @pets = {:fish =>,:cat =>,:dog =>}
+    @pets = {:fish => [],:cat => [],:dog => []}
     @@all << self
   end
 
