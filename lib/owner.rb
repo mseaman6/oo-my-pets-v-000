443ac661d14ea7d690ad550d19eb10b7d0a21f9f
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-22 17:39:52 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,20 @@
 class Owner
-  # code goes here
+
+  attr_reader :species
+  @@all = []
+
+  def initialize(name)
+    @species = "human"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+
 end
